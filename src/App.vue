c1d9f5e342e3fc430bb376c9a5e877c9d6c4a1e8
Doug Franklin <doug@dougfranklin.com>
2018-11-24 18:35:40 -0500

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <ion-page class="show-page">
       <ion-header>
         <ion-toolbar>
-          <ion-title>Ionic 4 + Vue</ion-title>
+          <ion-title>Ionic 4 + Vue A</ion-title>
         </ion-toolbar>
       </ion-header>
       <ion-content class="content" padding>
@@ -12,7 +12,22 @@
               <ion-label full>{{user.name}}</ion-label>
             </ion-item>
           </ion-list>
+          Hello 1
+          <ion-tabs>
+            <ion-tab>A</ion-tab>
+          </ion-tabs>
       </ion-content>
+      <ion-tabs>
+        <ion-tab label="Location" icon="navigate" href="/tabs/(location:location)">
+          <ion-router-outlet name="location"></ion-router-outlet>
+        </ion-tab>
+        <ion-tab label="My Details" icon="person" href="/tabs/(me:me)">
+          <ion-router-outlet name="me"></ion-router-outlet>
+        </ion-tab>
+        <ion-tab label="Camp Details" icon="bookmarks" href="/tabs/(camp:camp)">
+          <ion-router-outlet name="camp"></ion-router-outlet>
+        </ion-tab>
+      </ion-tabs>
     </ion-page>
   </ion-app>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-
 Vue.config.ignoredElements = [/^ion-/]
 
 new Vue({
